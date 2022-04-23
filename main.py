74537d716ac778524c7c50d894bac54c6900e51b
Martin Fidjeland <cer418@protonmail.com>
2022-04-23 08:21:02 +0100

    updated encoding/decoding using base64

--- main.py
+++ main.py
@@ -1,53 +1,29 @@
 import click
-import random
-
-keymap = {'a': '\U0001F38F', 'b': '\U0001F3C7', 'c': '\U0001F697', 'd': '\U0001F4C7', 'e': '\U0001F358',
-          'f': '\U0001F44B',
-          'g': '\U0001F68B', 'h': '\U0001F3C1', 'i': '\U0001F45C', 'j': '\U0001F573', 'k': '\U0001F52E',
-          'l': '\U0001F478',
-          'm': '\U0001F570', 'n': '\U0001F326', 'o': '\U0001F313', 'p': '\U0001F522', 'q': '\U0001F575',
-          'r': '\U0001F501', 's': '\U0001F636', 't': '\U0001F392', 'u': '\U0001F4C0', 'v': '\U0001F530',
-          'w': '\U0001F3B9',
-          'x': '\U0001F34A',
-          'y': '\U0001F61F', 'z': '\U0001F458',
-
-          '0': '\U0001F40A', '1': '\U0001F4E3', '2': '\U0001F3D3',
-          '3': '\U0001F42A', '4': '\U0001F966', '5': '\U0001F6A8', '6': '\U0001F6F6', '7': '\U0001F3A0',
-          '8': '\U0001F50D',
-          '9': '\U0001F48A',
-
-          '.': '\U0001F982', ',': '\U0001F4B3', '-': '\U0001F34E',
-          '?': '\U0001F32A', '!': '\U0001F62C', "'": '\U0001F356'}
+import base64
+import keymap
 
 
-def shuffle(text, key):
-    random.seed(key)
-    lst = list(range(len(text)))
-    random.shuffle(lst)
-    return lst
+def encrypt(text: str):
+    text = text.encode('utf-8')
+    return base64.b64encode(text).decode('utf-8')
+
+
+def decrypt(text: str):
+    return base64.b64decode(text).decode('utf-8')
 
 
 @click.command()
-@click.option('-t', '--text', prompt='Encrypt text', type=str)
-@click.option('-k', '--key', prompt='Encryption key', type=int)
-def encrypt(text: str, key: int):
-    try:
-        lst = shuffle(text, key)
-        shuffled_text = "".join([text[x] for x in lst])
-        print("".join(keymap.get(c, " ") for c in shuffled_text.lower()))
-    except ValueError:
-        print(f"Something went wrong encrypting the character(s): {text}")
-        pass
+@click.option('-t', '--text', prompt='Encode text')
+def encode(text):
+    text = encrypt(text)
+    print("".join(keymap.get(c, " ") for c in text))
 
 
 @click.command
-@click.option('-t', '--text', prompt='Decrypt text', type=str)
-@click.option('-k', '--key', prompt='Decryption key', type=int)
-def decrypt(text: str, key: int):
-    lst = shuffle(text, key)
-    deshuffled_text = "".join(text[i] for i, x in sorted(enumerate(lst), key=lambda x: x[1]))
+@click.option('-t', '--text', prompt='Decode text')
+def decode(text):
     mp = {v: k for k, v in keymap.items()}
-    print("".join(mp.get(c, " ") for c in deshuffled_text.lower()))
+    print(decrypt("".join(mp.get(c, " ") for c in text)))
 
 
 @click.group
@@ -55,8 +31,10 @@ def main():
     pass
 
 
-main.add_command(encrypt)
-main.add_command(decrypt)
+main.add_command(encode)
+main.add_command(decode)
+main.help = "Example: python main.py encode -t 'Hello World!'"
+main.add_help_option = False
 
 if __name__ == '__main__':
     main()
